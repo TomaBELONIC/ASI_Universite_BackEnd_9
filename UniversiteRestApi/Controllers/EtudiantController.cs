22c0ef3b5176d6b8046d71c35855b12abcceb9d7
Toma Belonic <toma.belonic@etud.u-picardie.fr>
2026-02-12 17:12:35 +0100

    Chapitre 5 - La couche Entry points

--- UniversiteRestApi/Controllers/EtudiantController.cs
+++ UniversiteRestApi/Controllers/EtudiantController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UniversiteDomain.DataAdapters.DataAdaptersFactory;
+using UniversiteDomain.Dtos;
 using UniversiteDomain.Entities;
 using UniversiteDomain.UseCases.EtudiantUseCases.Create;
 
@@ -25,21 +26,69 @@ namespace UniversiteRestApi.Controllers
             return "value";
         }
 
-        // POST api/<EtudiantController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        // // POST api/<EtudiantController>
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+        // }
         
         // Crée un nouvel étudiant sans parcours
+        // POST api/<EtudiantApi>
+        // [HttpPost]
+        // public async Task PostAsync([FromBody] Etudiant etudiant)
+        // {
+        //     CreateEtudiantUseCase uc=new CreateEtudiantUseCase(repositoryFactory);
+        //     await uc.ExecuteAsync(etudiant);
+        // }
+        
+        // Sans DTO
+        // Crée un nouvel étudiant sans parcours
+        // POST api/<EtudiantApi>
+        // [HttpPost]
+        // public async Task<Etudiant> PostAsync([FromBody] Etudiant etudiant)
+        // {
+        //     CreateEtudiantUseCase uc=new CreateEtudiantUseCase(repositoryFactory);
+        //     return await uc.ExecuteAsync(etudiant);
+        // }
+
+        // Maintenant en utilisant EtudiantDTO
+        // POST api/<EtudiantController>
+        // [HttpPost]
+        // public async Task<EtudiantDto> PostAsync([FromBody] EtudiantDto etudiantDto)
+        // {
+        //     // Converstion d'un EtudiantDto sans Id en Etudiant
+        //     Etudiant etudiant = etudiantDto.ToEntity();
+        //     // Création d'un étudiant
+        //     CreateEtudiantUseCase uc=new CreateEtudiantUseCase(repositoryFactory);
+        //     etudiant = await uc.ExecuteAsync(etudiant);
+        //     // Conversion de l'étudiant avec son nouvel Id en EtudiantDto
+        //     return new EtudiantDto().ToDto(etudiant);
+        // }
+        
+        // Avec EtudiantDTO et en personnalisant le retour des services
         // POST api/<EtudiantController>
         [HttpPost]
-        public async Task PostAsync([FromBody] Etudiant etudiant)
+        public async Task<ActionResult<EtudiantDto>> PostAsync([FromBody] EtudiantDto etudiantDto)
         {
-            CreateEtudiantUseCase uc=new CreateEtudiantUseCase(repositoryFactory);
-            await uc.ExecuteAsync(etudiant);
+            CreateEtudiantUseCase createEtudiantUc = new CreateEtudiantUseCase(repositoryFactory);           
+            Etudiant etud = etudiantDto.ToEntity();
+            try
+            {
+                etud = await createEtudiantUc.ExecuteAsync(etud);
+            }
+            catch (Exception e)
+            {
+                // On récupère ici les exceptions personnalisées définies dans la couche domain
+                // Et on les envoie avec le code d'erreur 400 et l'intitulé "erreurs de validation"
+                ModelState.AddModelError(nameof(e), e.Message);
+                return ValidationProblem();
+            }
+            EtudiantDto dto = new EtudiantDto().ToDto(etud);
+            // On revoie la route vers le get qu'on n'a pas encore écrit!
+            return CreatedAtAction(nameof(GetUnEtudiant), new { id = dto.Id }, dto);
         }
-
+        
+        
         // PUT api/<EtudiantController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
